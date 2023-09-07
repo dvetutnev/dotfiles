b21f0350507d48471603ec53843116a35ad3a10d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2023-09-07 16:30:01 +0300

    bash prompt

--- vulpecula.nix
+++ vulpecula.nix
@@ -43,6 +43,23 @@ let
           nohup $1 >/dev/null 2>&1 &
         }
       '';
+      initExtra = ''
+        # Provide a nice prompt if the terminal supports it.
+        if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+          PROMPT_COLOR="1;31m"
+          DIR_COLOR="1;34m"
+          ((UID)) && PROMPT_COLOR="1;32m"
+          if [ -n "$INSIDE_EMACS" ] || [ "$TERM" = "eterm" ] || [ "$TERM" = "eterm-color" ]; then
+            # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+            PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+          else
+            PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\[\033[$DIR_COLOR\]\w\[\033[$PROMPT_COLOR\]]\[\033[0m\]\$ "
+          fi
+          if test "$TERM" = "xterm"; then
+            PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+          fi
+        fi
+      '';
     };
     programs.dircolors.enable = true;
     home.shellAliases = {
