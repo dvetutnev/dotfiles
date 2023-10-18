b7ab6ee5887b2fd11236d3af2daee5c35a17ecec
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2023-10-19 01:33:39 +0300

    simplyfication promt

--- home.nix
+++ home.nix
@@ -42,12 +42,12 @@
           ((UID)) && PROMPT_COLOR="1;32m"
           if [ -n "$INSIDE_EMACS" ] || [ "$TERM" = "eterm" ] || [ "$TERM" = "eterm-color" ]; then
             # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-            PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+            PS1="\n\[\033[$PROMPT_COLOR\][\h:\w]\\$\[\033[0m\] "
           else
-            PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\[\033[$DIR_COLOR\]\w\[\033[$PROMPT_COLOR\]]\[\033[0m\]\$ "
+            PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\h: \w\a\]\h:\[\033[$DIR_COLOR\]\w\[\033[$PROMPT_COLOR\]]\[\033[0m\]\$ "
           fi
           if test "$TERM" = "xterm"; then
-            PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+            PS1="\[\033]2;\h:\w\007\]$PS1"
           fi
         fi
       '';
