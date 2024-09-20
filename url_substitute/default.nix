0cd80ae1967bcf01252d9c0c9bffe4b8f0aed6fc
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2024-09-20 16:09:52 +0300

    Refactoring

--- url_substitute/default.nix
+++ url_substitute/default.nix
@@ -1,14 +1,12 @@
 { lib }:
 replacements: url:
 let
-  findSubstitute = replacements: url:
-  let predicate = x: (builtins.match x.origin url) != null; in
-   lib.findFirst predicate null replacements;
-
-  replacement = findSubstitute replacements url;
+  matchUrl = x: builtins.match x.origin url;
+  predicate = x: (matchUrl x) != null;
+  substitute = lib.findFirst predicate null replacements;
 in
-  lib.throwIf (replacement == null) "Can`t replace url '${url}'"
+  lib.throwIf (substitute == null) "Can`t replace url '${url}'"
   {
-    url = builtins.replaceStrings [ "$1" ] (builtins.match replacement.origin url)
-      replacement.substitution;
+    url = builtins.replaceStrings [ "$1" ] (matchUrl substitute)
+      substitute.substitution;
   }
