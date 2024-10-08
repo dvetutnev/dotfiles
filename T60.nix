a683971659637de7173bfe5e88e1118b80ee9fd6
Dmitriy Vetutnev <dvetutnev@gmail.com>
2024-10-08 20:46:21 +0000

    clean

--- T60.nix
+++ T60.nix
@@ -5,25 +5,6 @@
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
     vim # or some other editor, e.g. nano or neovim
-
-    # Some common stuff that people expect to have
-    #procps
-    #killall
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
