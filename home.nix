5242433eda3938ce1daee8d41c14b30942441aac
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2024-11-03 18:20:53 +0300

    update

--- home.nix
+++ home.nix
@@ -90,7 +90,7 @@
     nix = {
       enable = true;
       package = pkgs.nix;
-      settings.experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      settings.experimental-features = [ "flakes" "nix-command" ];
     };
 
   };
