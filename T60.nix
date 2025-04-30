7854e4daf980e7330f33e3120c73e61753d15463
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-04-30 19:30:26 +0300

    which

--- T60.nix
+++ T60.nix
@@ -9,6 +9,7 @@
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
     # vim # or some other editor, e.g. nano or neovim
+    which
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
