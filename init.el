867f2574ebbb66db4f5567fbe25014c83c40d576
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-05-05 03:39:44 +0300

    emacs treesitter nix

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 (use-package solarized-theme
   :ensure t)
 (load-theme 'solarized-selenized-dark t)
+
 (unless (display-graphic-p)
   (menu-bar-mode -1))
+
+(use-package nix-mode
+  :ensure t
+  :mode "\\.nix\\'")
