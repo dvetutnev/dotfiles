7bb573e2330f3682bfd43a13ea853dc20a8a3589
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-06-28 18:23:27 +0300

    lynx qmk/via

--- lynx/configuration.nix
+++ lynx/configuration.nix
@@ -110,9 +110,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    via
   ];
+
+  hardware.keyboard.qmk.enable = true;
+  services.udev.packages = [ pkgs.via ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
