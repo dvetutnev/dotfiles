b644bd52cd5d52d384385af8ba3846382300aa51
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-07-07 00:16:49 +0300

    emacs: ido for bookmarks

--- init.el
+++ init.el
@@ -39,10 +39,25 @@
   :ensure t
   :defer t)
 
+
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
 (ido-mode t)
+
+(require 'bookmark)
+(require 'ido)
+
+(defun ido-bookmark-jump ()
+  "Uses ido to search for the bookmark"
+  (interactive)
+  (bookmark-jump
+   (bookmark-get-bookmark
+    (ido-completing-read "find bookmark: " (bookmark-all-names)))))
+
+(provide 'ido-bookmark-jump)
+
 (define-key (current-global-map) [remap bookmark-jump] 'ido-bookmark-jump)
+
 
 (use-package flycheck
   :ensure t
