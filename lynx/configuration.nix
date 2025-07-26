8c00fd49fed2a13ac774300c230758ec76767057
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-07-26 17:26:31 +0300

    lynx xmind

--- lynx/configuration.nix
+++ lynx/configuration.nix
@@ -107,10 +107,13 @@
     ];
   };
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     via
+    xmind
   ];
 
   hardware.keyboard.qmk.enable = true;
