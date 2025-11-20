93a9177bb7e24f490d040957edfbc6e25280ffd1
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-11-20 16:40:00 +0300

    clean kysa hardware configuration

--- kysa/hardware-configuration.nix
+++ kysa/hardware-configuration.nix
@@ -43,7 +43,4 @@
       size = 1024;
     }
   ];
-
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  virtualisation.hypervGuest.enable = true;
 }
