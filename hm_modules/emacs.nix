af16fed69cea61bac87d538f5800d44c3b39b127
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-22 18:37:58 +0300

    emacs grammars

--- hm_modules/emacs.nix
+++ hm_modules/emacs.nix
@@ -1,19 +1,26 @@
-{ pkgs
-, ...
+{
+  pkgs,
+  ...
 }:
 
 let
   emacs = pkgs.emacsWithPackagesFromUsePackage {
-      package = pkgs.emacs-unstable;
-      config = ../init.el;
-      defaultInitFile = true;
-      extraEmacsPackages = epkgs: with epkgs; [
+    package = pkgs.emacs-unstable;
+    config = ../init.el;
+    defaultInitFile = true;
+    extraEmacsPackages =
+      epkgs: with epkgs; [
         treesit-grammars.with-all-grammars
+        #(treesit-grammars.with-grammars (grammars: [ grammars.tree-sitter-scheme ]))
       ];
   };
 in
 {
   home.packages = [ emacs ];
-  home.sessionVariables = { EDITOR = "emacs"; };
-  home.shellAliases = { em = "emacs"; };
+  home.sessionVariables = {
+    EDITOR = "emacs";
+  };
+  home.shellAliases = {
+    em = "emacs";
+  };
 }
